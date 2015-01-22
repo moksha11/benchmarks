a908e5da258b62ca537d711dae9ef974d65068c9
moksha11 <sudarsun.kannan@gmail.com>
2015-01-22 14:10:00 -0500

    changes

--- linux-scalability-benchmark/mmapbench/mmapbench.c
+++ linux-scalability-benchmark/mmapbench/mmapbench.c
@@ -1,8 +1,8 @@
 /**
  * @file   mmap.c
- * @original author Wang Yuanxuan <zellux@gmail.com>
- * @modified by: sudarsun@gatech.edu
+ * @author Wang Yuanxuan <zellux@gmail.com>
  * @date   Fri Jan  8 21:23:31 2010
+ * 
  * @brief  An implementation of mmap bench mentioned in OSMark paper
  * 
  * 
@@ -20,16 +20,31 @@
 #include <sys/mman.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <assert.h>
 
 #include "config.h"
 #include "bench.h"
 
-int nbufs = 128000;
-//int nbufs = 64000;
+#define USENVMALLOC
+#ifdef USENVMALLOC
+#include <nv_map.h>
+#include <c_io.h>
+#define __NR_nv_mmap_pgoff 314
+#endif
+
+#define PROCID 112
+#define CHUNKID 112
+#define NOPERSIST 1
+
+//int nbufs = 256000;
+int nbufs = 64000;
 char *shared_area = NULL;
 int flag[32];
 int ncores = 1;
 char *filename = "/mnt/pmfs/shared.dat";
+
+
+
 
 void *
 worker(void *args)
@@ -40,9 +55,10 @@ worker(void *args)
 
     affinity_set(id);
 
-    for (i = 0; i < nbufs; i++)
+    for (i = 0; i < nbufs; i++){
+		shared_area[i *4096]=1;
         ret += shared_area[i *4096];
-    
+	}
     //printf("potato_test: thread#%d done.\n", core);
 
     return (void *) (long) ret;
@@ -58,8 +74,7 @@ void* setup_map_file(char *filepath, unsigned long bytes) {
         perror("Error opening file for writing");
         exit(-1);
     }
-    ftruncate(fd, bytes);
-    /*result = lseek(fd,bytes, SEEK_SET);
+    result = lseek(fd,bytes, SEEK_SET);
     if (result == -1) {
         close(fd);
         perror("Error calling lseek() to 'stretch' the file");
@@ -70,12 +85,31 @@ void* setup_map_file(char *filepath, unsigned long bytes) {
         close(fd);
         perror("Error writing last byte of the file");
         exit(-1);
-    }*/
+    }
 	return fd;
 }
 
-int
-main(int argc, char **argv)
+#ifdef USENVMALLOC
+void* map_using_mmap(size_t size){
+
+	char *map;
+	int proc_id = PROCID;
+    int chunk_id =CHUNKID;
+    struct nvmap_arg_struct a;
+
+	a.fd = -1;
+    a.offset = 0;
+    a.vma_id =chunk_id;
+    a.proc_id = proc_id;
+    a.pflags = 1;
+    a.noPersist = NOPERSIST;
+	map = (char *) syscall(__NR_nv_mmap_pgoff, 0, size,  PROT_READ | PROT_WRITE, MAP_PRIVATE| MAP_ANONYMOUS, &a);
+	//map = (char *) mmap(0, size, PROT_READ, MAP_PRIVATE| MAP_ANONYMOUS,-1, 0);
+	return map;
+}
+#endif
+
+int main(int argc, char **argv)
 {
     int i, fd;
     pthread_t tid[32];
@@ -89,9 +123,19 @@ main(int argc, char **argv)
         ncores = atoi(argv[1]);
     }
 
+    if (argc > 2) {
+        nbufs = atoi(argv[2]);
+    }
+
+
+#ifndef USENVMALLOC
 	fd = setup_map_file(filename, (nbufs+1)*4096);
     //fd = open(filename, O_RDONLY);
-    shared_area = mmap(0, (1 + nbufs) * 4096, PROT_READ, MAP_PRIVATE, fd, 0);
+    shared_area = mmap(0, (1 + nbufs) * 4096, PROT_READ|PROT_WRITE, MAP_PRIVATE, fd, 0);
+#else
+	shared_area = map_using_mmap((1 + nbufs) * 4096);
+#endif
+	assert(shared_area != MAP_FAILED);
     
     start = read_tsc();
     for (i = 0; i < ncores; i++) {
--- linux-scalability-benchmark/mmapbench/runtest.py
+++ linux-scalability-benchmark/mmapbench/runtest.py
@@ -3,7 +3,9 @@
 import os, re
 from subprocess import *
 
-corelist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+#corelist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+corelist = [1,2,3,4]
+pagecntlist = [32000, 64000, 128000, 256000]
 repeat = 5
 
 def warmup():
@@ -16,14 +18,20 @@ def test():
 	print('Begin testing...')
 	pattern = re.compile(r'usec: (\d+)')
 	for n in corelist:
-		total = 0
-		for i in xrange(repeat):
-			p = Popen('./mmapbench %d' % n, shell=True, stdout=PIPE)
-			os.waitpid(p.pid, 0)
-			output = p.stdout.read().strip()
-			usec = int(pattern.search(output).group(1))
-			total += usec
-		print('Core #%d: %d' % (n, total / repeat))
+		for pg in pagecntlist:
+			total = 0
+			for i in xrange(repeat):
+				os.system("rm -rf /mnt/pmfs/*")
+				p = Popen('./mmapbench %d %d' % (n,  pg), shell=True, stdout=PIPE)
+				#p = Popen('./mmapbench %d %d' % pg, shell=True, stdout=PIPE)
+				#p=Popen(["./mmapbench",str(n), str(pg)], shell=True, stdout=PIPE)
+				#p=Popen(["./mmapbench",str(n), str(pg)])
+				os.waitpid(p.pid, 0)
+				output = p.stdout.read().strip()
+				usec = int(pattern.search(output).group(1))
+				total += usec
+  			print('Core #%d: Pages #%d:  %d' % (n, pg, total / repeat))
+				#print('Core #%d: %d' % (n, usec))
 
 warmup()
 test()
